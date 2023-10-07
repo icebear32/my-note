ffba529b573b447cd56c0c924f3914ff0dc1c82c
icebear32 <1727693137@qq.com>
2023-10-08 00:00:10 +0800

    my-note项目搭建

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-button>naive-ui</n-button>
+  <n-icon size="30" :component="AcUnitRound"></n-icon>
+</template>
+
+<script setup>
+import { AcUnitRound } from '@vicons/material'
+</script>
+
+<style scoped>
+</style>
