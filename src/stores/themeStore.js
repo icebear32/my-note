fb6267ea25b76ef1b0be89fc94406ee94d04e462
icebear32 <1727693137@qq.com>
2023-10-08 13:52:21 +0800

    暗系、亮系主题切换的优化，持久化

--- src/stores/themeStore.js
+++ src/stores/themeStore.js
@@ -4,34 +4,43 @@ import { computed, ref } from 'vue'
 import { DarkModeRound, LightbulbOutlined } from '@vicons/material'
 
 // 关于主题的全局状态
-export const useThemeStore = defineStore("theme", () => {
-  // 是否是暗系主题
-  const isDarkTheme = ref(false)
+export const useThemeStore = defineStore(
+  "theme",
+  () => {
+    // 是否是暗系主题
+    const isDarkTheme = ref(false)
 
-  // 暗系主题-灯泡，亮系主题-月牙
-  const theme = computed(() => {
-    if (isDarkTheme.value) {
-      // 暗系主题
-      return {
-        name: darkTheme,
-        icon: LightbulbOutlined
+    // 暗系主题-灯泡，亮系主题-月牙
+    const theme = computed(() => {
+      if (isDarkTheme.value) {
+        // 暗系主题
+        return {
+          name: darkTheme,
+          icon: LightbulbOutlined
+        }
+      } else {
+        // 亮系主题
+        return {
+          name: null,
+          icon: DarkModeRound
+        }
       }
-    } else {
-      // 亮系主题
-      return {
-        name: null,
-        icon: DarkModeRound
-      }
+    })
+
+    /**
+     * 更改主题
+     * @param {Boolean} dark 是否是暗系 
+     */
+    const changeTheme = dark => {
+      isDarkTheme.value = dark
     }
-  })
 
-  /**
-   * 更改主题
-   * @param {Boolean} dark 是否是暗系 
-   */
-  const changeTheme = dark => {
-    isDarkTheme.value = dark
+    return { isDarkTheme, theme, changeTheme }
+  },
+  {
+    persist: {
+      storage: localStorage, // 本地存储
+      paths: ["isDarkTheme"], // 将 isDarkTheme 持久化
+    }
   }
-
-  return { isDarkTheme, theme, changeTheme }
-})
+)
