4515bda221e1188af0810fa23baf54ec4c603245
icebear32 <1727693137@qq.com>
2023-10-08 12:03:26 +0800

    应用头部布局

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
+import MainTopToolbar from '@/components/toolbar/MainTopToolbar.vue';
 </script>
 
 <template>
   <n-layout position="absolute" style="border: 1px solid black;">
+    <!-- 应用头 -->
     <n-layout-header bordered style="height: 64px;padding: 0 20px;">
-      颐和园路
+      <MainTopToolbar />
     </n-layout-header>
     <n-layout position="absolute" style="top: 64px;" has-sider>
+      <!-- 左侧应用栏目 -->
       <n-layout-sider bordered width="64px" content-style="padding: 24px 0;text-align: center;">
         海淀桥
       </n-layout-sider>
+      <!-- 主页面 -->
       <n-layout-content content-style="padding: 24px;">
         平山道
       </n-layout-content>
---
+++ src/components/toolbar/MainTopToolbar.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { DarkModeRound, NotificationsNoneOutlined } from '@vicons/material'
+</script>
+
+<template>
+    <n-space align="center" justify="space-between" style="height: 100%;">
+        <n-text type="info">
+            ich笔记
+        </n-text>
+        <n-space>
+            <!-- 头像 -->
+            <n-avatar round size="small" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+                style="position: relative;top: 3px;" />
+
+            <!-- 分割线 -->
+            <n-divider vertical style="position: relative;top: 5px;" />
+
+            <!-- 消息 -->
+            <n-badge dot processing type="success" :offset="[-8, 4]">
+                <n-button circle tertiary>
+                    <n-icon size="18" :component="NotificationsNoneOutlined"></n-icon>
+                </n-button>
+            </n-badge>
+
+            <!-- 主题按钮 -->
+            <n-button circle tertiary>
+                <n-icon size="18" :component="DarkModeRound"></n-icon>
+            </n-button>
+
+            <!-- 登录按钮 -->
+            <n-button tertiary type="primary">登录</n-button>
+        </n-space>
+    </n-space>
+</template>
