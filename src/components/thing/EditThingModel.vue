bea9c51a4781f93e06172de822f75a83c4cbe1a1
icebear32 <1727693137@qq.com>
2023-11-08 20:57:36 +0800

    优化编辑小记组件（骨架屏）

--- src/components/thing/EditThingModel.vue
+++ src/components/thing/EditThingModel.vue
@@ -76,84 +76,131 @@ const saveEditThing = () => {
         }
     })
 }
+
+// 是否处于加载中
+const loading = ref(true)
 </script>
 
 <template>
     <n-modal :show="true">
-        <n-card size="small" :bordered="false" style="width: 460px;" :class="{ 'thing-card-finished': formValue.finish }">
-            <!-- 内容栏 -->
-            <template #default>
-                <n-form ref="formRef" :model="formValue" :rules="formRules">
-                    <n-form-item path="title">
+        <div>
+            <!-- 骨架屏卡片 -->
+            <n-card v-show="loading" size="small" :bordered="false" style="width: 460px;">
+                <template #default>
+                    <div style="padding: 0 14px;">
                         <!-- 小记标题 -->
-                        <n-input v-model:value="formValue.title" size="large" placeholder="请输入小记标题"
-                            style="--n-border: none; background-color: transparent;" />
-                    </n-form-item>
+                        <n-skeleton :height="40" :sharp="false"></n-skeleton>
+                    </div>
                     <!-- 置顶、标签容器、分割线、待办事项 -->
                     <div style="padding: 10px 14px 0;">
                         <!-- 置顶、标签容器 -->
                         <n-space align="center">
-                            <n-text depth="3">置顶：</n-text>
-                            <n-switch v-model:value="formValue.top" :round="false" />
+                            <!-- 置顶文本 -->
+                            <n-skeleton :width="42" :height="22"></n-skeleton>
+                            <!-- 置顶开关 -->
+                            <n-skeleton :width="40" :height="22" :sharp="false"></n-skeleton>
                             <!-- 标签容器 -->
                             <n-space align="center">
-                                <n-form-item :show-label="false" :show-feedback="false" path="tags">
-                                    <n-text depth="3">标签：</n-text>
-                                    <n-dynamic-tags v-model:value="formValue.tags" :max="5"
-                                        :color="{ borderColor: 'rgba(0, 0, 0, 0)' }" />
-                                </n-form-item>
+                                <!-- 标签文本 -->
+                                <n-skeleton :width="42" :height="14"></n-skeleton>
+                                <!-- 标签创建开关 -->
+                                <n-skeleton :width="38" :height="28" :sharp="false"></n-skeleton>
                             </n-space>
                         </n-space>
-
                         <!-- 分割线 -->
                         <n-divider style="margin-top: 14px;"></n-divider>
-
                         <!-- 待办事项列表 -->
-                        <n-form-item :show-label="false" :show-feedback="false" path="content">
-                            <n-dynamic-input v-model:value="formValue.content" :on-create="onCreateTuDoThing">
-                                <template #create-button-default>
-                                    添加一个待办事项
-                                </template>
-                                <template #default="{ value }">
-                                    <div style="display: flex; align-items: center; width: 100%">
-                                        <!-- 复选框（是否完成这个待办事项） -->
-                                        <n-checkbox v-model:checked="value.checked" />
-                                        <!-- 输入框（待办事项） -->
-                                        <n-input v-model:value="value.thing" placeholder="请输入......"
-                                            style="margin-left: 12px; --n-border: none;" />
-                                    </div>
-                                </template>
-                                <template #action="{ index, create, remove, move }">
-                                    <div style="display: flex; align-items: center; margin-left: 12px;">
-                                        <!-- 添加按钮 -->
-                                        <n-button circle tertiary type="tertiary" @click="() => create(index)"
-                                            style="margin-right: 6px;">
-                                            <n-icon :component="AddBoxRound"></n-icon>
-                                        </n-button>
-                                        <!-- 删除按钮 -->
-                                        <n-button circle tertiary type="tertiary" @click="() => remove(index)">
-                                            <n-icon :component="DeleteForeverFilled"></n-icon>
-                                        </n-button>
-                                    </div>
-                                </template>
-                            </n-dynamic-input>
-                        </n-form-item>
+                        <n-skeleton :height="34" :sharp="false"></n-skeleton>
                     </div>
-                </n-form>
-            </template>
+                </template>
+                <template #action>
+                    <n-grid cols="2" :x-gap="12">
+                        <n-gi>
+                            <n-skeleton :height="34" :sharp="false"></n-skeleton>
+                        </n-gi>
+                        <n-gi>
+                            <n-skeleton :height="34" :sharp="false"></n-skeleton>
+                        </n-gi>
+                    </n-grid>
+                </template>
+            </n-card>
+            <!-- 编辑小记表单卡片 -->
+            <n-card v-show="!loading" size="small" :bordered="false" style="width: 460px;"
+                :class="{ 'thing-card-finished': formValue.finish }">
+                <!-- 内容栏 -->
+                <template #default>
+                    <n-form ref="formRef" :model="formValue" :rules="formRules">
+                        <n-form-item path="title">
+                            <!-- 小记标题 -->
+                            <n-input v-model:value="formValue.title" size="large" placeholder="请输入小记标题"
+                                style="--n-border: none; background-color: transparent;" />
+                        </n-form-item>
+                        <!-- 置顶、标签容器、分割线、待办事项 -->
+                        <div style="padding: 10px 14px 0;">
+                            <!-- 置顶、标签容器 -->
+                            <n-space align="center">
+                                <n-text depth="3">置顶：</n-text>
+                                <n-switch v-model:value="formValue.top" :round="false" />
+                                <!-- 标签容器 -->
+                                <n-space align="center">
+                                    <n-form-item :show-label="false" :show-feedback="false" path="tags">
+                                        <n-text depth="3">标签：</n-text>
+                                        <n-dynamic-tags v-model:value="formValue.tags" :max="5"
+                                            :color="{ borderColor: 'rgba(0, 0, 0, 0)' }" />
+                                    </n-form-item>
+                                </n-space>
+                            </n-space>
 
-            <!-- 功能栏 -->
-            <template #action>
-                <n-grid cols="2" :x-gap="12">
-                    <n-gi>
-                        <n-button block tertiary>取消</n-button>
-                    </n-gi>
-                    <n-gi>
-                        <n-button block ghost type="primary" @click="saveEditThing">保存</n-button>
-                    </n-gi>
-                </n-grid>
-            </template>
-        </n-card>
+                            <!-- 分割线 -->
+                            <n-divider style="margin-top: 14px;"></n-divider>
+
+                            <!-- 待办事项列表 -->
+                            <n-form-item :show-label="false" :show-feedback="false" path="content">
+                                <n-dynamic-input v-model:value="formValue.content" :on-create="onCreateTuDoThing">
+                                    <template #create-button-default>
+                                        添加一个待办事项
+                                    </template>
+                                    <template #default="{ value }">
+                                        <div style="display: flex; align-items: center; width: 100%">
+                                            <!-- 复选框（是否完成这个待办事项） -->
+                                            <n-checkbox v-model:checked="value.checked" />
+                                            <!-- 输入框（待办事项） -->
+                                            <n-input v-model:value="value.thing" placeholder="请输入......"
+                                                style="margin-left: 12px; --n-border: none;" />
+                                        </div>
+                                    </template>
+                                    <template #action="{ index, create, remove, move }">
+                                        <div style="display: flex; align-items: center; margin-left: 12px;">
+                                            <!-- 添加按钮 -->
+                                            <n-button circle tertiary type="tertiary" @click="() => create(index)"
+                                                style="margin-right: 6px;">
+                                                <n-icon :component="AddBoxRound"></n-icon>
+                                            </n-button>
+                                            <!-- 删除按钮 -->
+                                            <n-button circle tertiary type="tertiary" @click="() => remove(index)">
+                                                <n-icon :component="DeleteForeverFilled"></n-icon>
+                                            </n-button>
+                                        </div>
+                                    </template>
+                                </n-dynamic-input>
+                            </n-form-item>
+                        </div>
+                    </n-form>
+                </template>
+
+                <!-- 功能栏 -->
+                <template #action>
+                    <n-grid cols="2" :x-gap="12">
+                        <n-gi>
+                            <n-button block tertiary>取消</n-button>
+                        </n-gi>
+                        <n-gi>
+                            <n-button block ghost type="primary" @click="saveEditThing">保存</n-button>
+                        </n-gi>
+                    </n-grid>
+                </template>
+            </n-card>
+        </div>
     </n-modal>
 </template>
 
